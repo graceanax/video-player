07640006489172d161ef983ad68a32463df28e45
graceana <graceanax@gmail.com>
2022-02-21 23:39:26 -0600

    add progress bar functionallity

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const $play = document.querySelector('#play');
 const $pause = document.querySelector('#pause');
 const $backward = document.querySelector('#backward');
 const $forward = document.querySelector('#forward');
+const $progress = document.querySelector('#progress');
 
 $play.addEventListener('click', handlePlay);
 $pause.addEventListener('click', handlePause);
@@ -28,4 +29,23 @@ function handleForward() {
   $video.currentTime += 10;
   $pause.hidden = true;
   $play.hidden = false;
+}
+
+$video.addEventListener('loadedmetadata', handleLoaded);
+$video.addEventListener('timeupdate', handleTimeUpdate);
+
+function handleLoaded () {
+  $progress.max = $video.duration;
+}
+
+function handleTimeUpdate () {
+  $progress.value = $video.currentTime;
+  $progress.max = $video.duration;
+}
+
+$progress.addEventListener('input', handleInput);
+
+function handleInput() {
+  $progress.max = $video.duration;
+  $video.currentTime = $progress.value;
 }
